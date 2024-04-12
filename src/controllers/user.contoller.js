2b64181973a4fe0ed964ea81e5fe0939cd24a49a
ChandraShekhar <csyadav0513@gmail.com>
2024-04-12 23:35:16 +0530

    Refactor file upload logic in user.controller.js

--- src/controllers/user.contoller.js
+++ src/controllers/user.contoller.js
@@ -35,9 +35,18 @@ const registerUser = asyncHandler(async (req, res) => {
   }
   // check for images, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+  // check for cover image
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files?.coverImage[0]?.path;
+  }
 
+  // check for avatar
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required 🖼️");
   }
